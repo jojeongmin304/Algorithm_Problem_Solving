ccc33431f6c54e86b3e916de2dd9388d485e2cbd
jojeongmin304 <140388077+jojeongmin304@users.noreply.github.com>
2025-01-02 12:20:51 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.59 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -5,33 +5,26 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0;
-    
-    queue<int> q;
-    for (int& i : truck_weights)
-        q.push(i);
-    
+    int answer = bridge_length;
     queue<int> bridge;
     int nowWeight = 0;
-    while(!q.empty()) {
-        answer++;
-        
-        if(bridge.size() == bridge_length) {  //다리끝 맨앞트럭 나과는과정
+    
+    for (int i = 0; i < truck_weights.size(); i++) {
+        if (!bridge.empty() && bridge.size() == bridge_length) {
             nowWeight -= bridge.front();
             bridge.pop();
         }
         
-        if (weight >= nowWeight + q.front()) { //대기열에서 다리로
-            if (q.size() == 1)
-                answer += bridge_length; //마지막트럭이 다리 건너는걸 고려
-            
-            nowWeight += q.front();
-            bridge.push(q.front());
-            q.pop();
+        
+        if (nowWeight + truck_weights[i] <= weight) {
+            bridge.push(truck_weights[i]);
+            nowWeight += truck_weights[i];
         }
         else {
             bridge.push(0);
+            i--;
         }
+        answer++;
     }
     return answer;
 }
