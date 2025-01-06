c967205074bf53b832040be9792d7b25853d6465
jojeongmin304 <140388077+jojeongmin304@users.noreply.github.com>
2025-01-06 12:21:29 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,21 @@
+def solution(n, lost, reserve):
+    answer = n - len(lost)
+    lost.sort()
+    
+    if(len(reserve) == 0): return n - len(lost)
+    
+    for student in lost[:]:
+        if student in reserve:
+            reserve.remove(student)
+            lost.remove(student)
+            answer += 1
+            
+    for student in lost[:]:
+        if student-1 in reserve:
+            reserve.remove(student-1)
+            answer += 1
+        elif student+1 in reserve:
+            reserve.remove(student+1)
+            answer += 1
+    
+    return answer
