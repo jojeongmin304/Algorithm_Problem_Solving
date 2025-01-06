648b2194d422fd594b470ca0f0700b63ce000810
jojeongmin304 <140388077+jojeongmin304@users.noreply.github.com>
2025-01-06 11:46:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.81 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,14 @@
 def solution(participant, completion):
     answer = ''
-    d = dict();
+    d = dict()
     for name in completion:
         if name in d:
-            d[name] += 1; 
+            d[name] += 1
         else:
-            d[name] = 1;
+            d[name] = 1
         
-    for name in participant:  
+    for name in participant:
         if name in d and d[name] > 0:
-            d[name] -= 1;
-        else :
-            return name;
+            d[name] -= 1
+        else:
+            return name
