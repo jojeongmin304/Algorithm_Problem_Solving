8e724a894a85dd272e6806a56d5b3fad3fb71404
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-01-30 15:07:20 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,18 @@
+import math
+
 def solution(progresses, speeds):
-    import math
     answer = []
-    n = len(progresses)
-    
-    for i in range(0, n):
-        progresses[i] = math.ceil((100-progresses[i]) / speeds[i])
-    
-    max = progresses[0]
-    count = 1
-    for i in range(1, n):
-        if max < progresses[i]:
-            max = progresses[i]
+    for i in range(0, len(speeds)):
+        left = (99-progresses[i]) / speeds[i] + 1
+        progresses[i] = math.floor(left)
+        
+    start = progresses[0]
+    count = 0
+    for i in range(0, len(speeds)):
+        if (progresses[i] > start):
             answer.append(count)
-            count = 1
-        else:
-            count += 1
+            count = 0
+            start = progresses[i]
+        count += 1
     answer.append(count)
-            
     return answer
