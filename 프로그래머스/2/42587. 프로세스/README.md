5a6730666416a195492cdc2c72e547c4b07a8eaa
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-01-31 11:11:34 +0900

    [level 2] Title: 프로세스, Time: 0.78 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.97 ms
+메모리: 10.3 MB, 시간: 0.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 16일 20:15:06
+2025년 01월 31일 11:11:32
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,17 @@
 def solution(priorities, location):
-    answer = 1
+    answer = 0
     while True:
         if priorities[0] == max(priorities):
-            if location == 0:
-                break
-            del priorities[0]
-            location -= 1
             answer += 1
+            if location == 0:
+                return answer
+            else:
+                priorities.remove(priorities[0])
+                location -= 1
         else:
             priorities.append(priorities[0])
-            del priorities[0]
+            priorities.remove(priorities[0])
             if location == 0:
                 location += len(priorities)-1
             else:
                 location -= 1
-    return answer
