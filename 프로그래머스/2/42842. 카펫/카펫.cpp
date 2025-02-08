7d72ab08d363a5256e13cc29d3ea8e2d28f91dba
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-08 10:54:49 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -4,22 +4,16 @@
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
     int width = 1;
-    int height = yellow / width;  //노란색의 가로세로
-    
-    while(true) {
-        if (!(yellow % width)) {
+    int height = yellow / width;
+    while (true) {
+        if (yellow % width == 0){
             height = yellow / width;
             
-            if (width >= height && (width+2)*(height+2) - yellow == brown)
-                break;
+            if (width >= height and (width+2)*(height+2) == brown+yellow)
+            break;
         }
         width++;
     }
-        
-    answer.push_back(width+2);
-    answer.push_back(height+2);
-    
-    return answer;
+    return {width+2, height+2};
 }
