ec73d40b5a79b8a3c569e9e2367b011da4a3c773
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-09 18:34:35 +0900

    [level 1] Title: 모의고사, Time: 2.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 4.01 MB, 시간: 0.13 ms
+메모리: 10.3 MB, 시간: 2.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 17:53:50
+2025년 02월 09일 18:34:33
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,28 +1,15 @@
 def solution(answers):
     answer = []
-    
-    answerOne = [1, 2, 3, 4, 5]
-    countOne = 0
-    
-    answerTwo = [2, 1, 2, 3, 2, 4, 2, 5]
-    countTwo = 0
-    
-    answerThree = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    countThree = 0
-    
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
     for i in range(0, len(answers)):
-        if (answers[i] == answerOne[i % 5]): countOne += 1;
-        if (answers[i] == answerTwo[i % 8]): countTwo += 1;
-        if (answers[i] == answerThree[i % 10]): countThree += 1; 
+        if (answers[i] == first[i % 5]): score[0] += 1
+        if (answers[i] == second[i % 8]): score[1] += 1
+        if (answers[i] == third[i % 10]): score[2] += 1
     
-    temp = []
-    temp.append(countOne);
-    temp.append(countTwo);
-    temp.append(countThree);
-    maxScore = max(temp)
-    
-    for i in range(0,3):
-        if (maxScore == temp[i]):
+    for i in range(0, len(score)):
+        if(max(score) == score[i]):
             answer.append(i+1)
-        
     return answer
