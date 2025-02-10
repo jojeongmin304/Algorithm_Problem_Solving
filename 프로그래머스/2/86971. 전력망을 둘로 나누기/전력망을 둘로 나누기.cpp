ed39e5dd0ffce70783be4c1ae178da500aaafdd5
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-10 17:33:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.10 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,44 +1,45 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <math.h>
+#include <cmath>
+
 using namespace std;
+
+int BFS(int a, int b, vector<vector<int>>& adj) {
+    queue<int> q;
+    vector<int> visited(101, 0);
+    visited[a] = 1;
+    q.push(a);
+    int count = 1;
+    
+    while(!q.empty()) {
+        int current = q.front(); q.pop();
+        if (adj[current].size() == 0) continue;
+        for (int& node : adj[current])
+            if (visited[node] == 0 && node != b) {
+                visited[node]++;
+                q.push(node);
+                count++;
+            }
+    }
+    return count;
+}
 
 int solution(int n, vector<vector<int>> wires) {
     int answer = n;
     vector<vector<int>> adjList(n+1);
-    for (const auto& wire: wires) {
+    
+    for (const auto& wire : wires) {
         adjList[wire[0]].push_back(wire[1]);
         adjList[wire[1]].push_back(wire[0]);
     }
     
-    for (auto& wire: wires) {
-        queue<int> q;
-        vector<int> visited(n+1, 0);
-        int count = 1;
-        visited[wire[0]] = 1;
-        for (int& i : adjList[wire[0]])
-            if (i != wire[1] && !visited[i]) {
-                visited[i] = 1;
-                q.push(i);
-                count++;
-            }
-        while(!q.empty()) {
-            int current = q.front(); q.pop();
-            
-            for (auto& i : adjList[current]) {
-                if (visited[i] != 1) {
-                    visited[i] = 1;
-                    q.push(i);
-                    count++;
-                }
-            }
-        }
-        int rhs = count;
-        int lhs = n - rhs;
-        int gap = abs(rhs - lhs);
-        if (gap < answer)
-            answer = gap;
+    for (const auto& wire : wires) {
+        int left = BFS(wire[0], wire[1], adjList);
+        int right = n - left;
+        if (abs(left-right) < answer)
+            answer = abs(left-right);
     }
+    
     return answer;
 }
