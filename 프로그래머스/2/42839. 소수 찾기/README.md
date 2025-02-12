c14716c7bb666ee9cc47d95a62ad779cd8259823
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-12 17:39:11 +0900

    [level 2] Title: 소수 찾기, Time: 0.62 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 12 MB, 시간: 12.82 ms
+메모리: 4.18 MB, 시간: 0.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 11:26:47
+2025년 02월 12일 17:39:10
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,42 +1,34 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <math.h>
 #include <set>
+#include <cmath>
 
 using namespace std;
 
-bool isPrime(int n) 
-{
-    if (n < 2)
+bool isPrime(int num) {
+    if (num < 2)
         return false;
-    for (int i = 2; i <= sqrt(n); ++i) {
-        if (n % i == 0)
-            return false;
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) return false;
     }
     return true;
 }
 
 int solution(string numbers) {
     int answer = 0;
-    set<int> ans{};
+    vector<int> temp;
+    set<int> a{};
     
     sort(numbers.begin(), numbers.end());
-    do
-    {
-        string tmp;
-        for (int i = 0; i < numbers.size(); ++i) {
-            tmp += numbers[i];
-            if (isPrime(stoi(tmp)))
-                ans.insert(stoi(tmp));
+    do {
+        string str = "";
+        for (const auto& c : numbers) {
+            str += c;
+            if (isPrime(stoi(str)))
+                a.insert(stoi(str));
         }
-    }
+    }while(next_permutation(numbers.begin(), numbers.end()));
     
-    while(next_permutation(numbers.begin(), numbers.end()));
-    
-    answer = ans.size();
-    
-    
-    
-    return answer;
+    return a.size();
 }
