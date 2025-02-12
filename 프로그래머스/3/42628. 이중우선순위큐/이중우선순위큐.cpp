4c3299bc792f58199e9fcedb09d7d88278a59f24
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-12 20:09:06 +0900

    [level 3] Title: 이중우선순위큐, Time: 19.69 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -9,43 +9,43 @@ vector<int> solution(vector<string> operations) {
     vector<int> answer;
     priority_queue<int> maxq;
     priority_queue<int, vector<int>, greater<int>> minq;
-    unordered_map<int, int> count;
+    unordered_map<int, int> um;
     int size = 0;
     
     for (const auto& op : operations) {
         if (op[0] == 'I') {
-            maxq.push(stoi(op.substr(2)));
-            minq.push(stoi(op.substr(2)));
-            count[stoi(op.substr(2))]++;
+            int num = stoi(op.substr(2));
+            maxq.push(num);
+            minq.push(num);
+            um[num]++;
             size++;
         }
         else if (op[0] == 'D' && size > 0) {
             if (op[2] == '1') {
-                while(!maxq.empty() && count[maxq.top()] == 0)
+                while (!maxq.empty() && um[maxq.top()] == 0)
                     maxq.pop();
                 if (!maxq.empty()) {
-                    count[maxq.top()]--;
+                    um[maxq.top()]--;
                     maxq.pop();
                     size--;
                 }
             }
             else {
-                while(!minq.empty() && count[minq.top()] == 0)
+                while (!minq.empty() && um[minq.top()] == 0)
                     minq.pop();
                 if (!minq.empty()) {
-                    count[minq.top()]--;
+                    um[minq.top()]--;
                     minq.pop();
                     size--;
                 }
             }
         }
     }
-    while(!maxq.empty() && count[maxq.top()] == 0)
+    while (!maxq.empty() && um[maxq.top()] == 0)
         maxq.pop();
-    while(!minq.empty() && count[minq.top()] == 0)
+    while (!minq.empty() && um[minq.top()] == 0)
         minq.pop();
     
-    if (size == 0) return{0, 0};
-    
+    if (size == 0) return {0, 0};
     return {maxq.top(), minq.top()};
 }
