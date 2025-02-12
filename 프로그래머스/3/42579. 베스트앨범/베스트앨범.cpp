496012225b97a5f872e44f1db65f1306185eecff
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-12 16:41:08 +0900

    [level 3] Title: 베스트앨범, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -18,7 +18,7 @@ bool songcmp(pair<int, int>& a, pair<int, int>& b) {
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     unordered_map<string, int> genres_dict;
-    unordered_map<string, vector<pair<int,int>>> songs_dict;
+    unordered_map<string, vector<pair<int, int>>> songs_dict;
     
     for (int i = 0; i < genres.size(); i++) {
         genres_dict[genres[i]] += plays[i];
@@ -26,13 +26,14 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     }
     
     vector<pair<string, int>> sorted_genres(genres_dict.begin(), genres_dict.end());
+    
     sort(sorted_genres.begin(), sorted_genres.end(), gencmp);
     
-    for (auto& genre : sorted_genres) {
-        sort(songs_dict[genre.first].begin(), songs_dict[genre.first].end(),songcmp);
-        for (int i = 0; i < min(2, (int)songs_dict[genre.first].size()); i++) {
-            answer.push_back(songs_dict[genre.first][i].first);
-        }
+    for (auto& pair : sorted_genres) {
+        auto& songs = songs_dict[pair.first];
+        sort(songs.begin(), songs.end(), songcmp);
+        for (int i = 0; i < min(2, (int)songs.size()); i++)
+            answer.push_back(songs[i].first);
     }
     
     return answer;
