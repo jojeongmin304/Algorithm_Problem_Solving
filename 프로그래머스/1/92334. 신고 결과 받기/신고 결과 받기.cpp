114672dc8cbb43a448bd288133e16f00d05fa2f6
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-13 12:29:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 257.12 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -1,28 +1,27 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <unordered_map>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer;
-    unordered_map<string, unordered_set<string>> reported_user;
     unordered_map<string, int> count;
+    unordered_map<string, unordered_set<string>> reported_user;
     
-    for (string& r : report) {
-        stringstream ss(r);
+    for (string& str : report) {
+        stringstream ss(str);
         string user_id, reported_id;
         ss >> user_id >> reported_id;
         reported_user[reported_id].insert(user_id);
     }
     
-    for (auto &[reported_id, user_id_lst] : reported_user) {
+    for (auto& [reported_id, user_id_lst] : reported_user) {
         if (user_id_lst.size() >= k) {
-            for (const string& uid : user_id_lst) {
-                count[uid]++;  //신고들어갔다고 메일 보내기
-            }
+            for (const string& uid : user_id_lst)
+                count[uid]++;
         }
     }
     
