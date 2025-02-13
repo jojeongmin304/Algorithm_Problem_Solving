5aeddd3569025bc2924f65997737dfb1faeeedb0
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-13 11:56:37 +0900

    [level 2] Title: 큰 수 만들기, Time: 3.73 ms, Memory: 5.61 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -6,21 +6,23 @@ using namespace std;
 string solution(string number, int k) {
     string answer = "";
     vector<char> stack;
-    for (char& c : number) {
-        while (!stack.empty() && stack.back() < c) {
+    
+    for (const auto& c : number) {
+        while(stack.size() > 0 && c > stack.back()) {
             if (k > 0) {
                 stack.pop_back();
                 k--;
             }
-            else
-                break;
+            else break;
         }
         stack.push_back(c);
     }
-    while (k > 0) {
+    while (k > 0 && stack.size() > 0) {
         stack.pop_back();
         k--;
     }
     
-    return string(stack.begin(), stack.end());
+    answer = string(stack.begin(), stack.end());
+    if (answer[0] == '0') return "0";
+    return answer;
 }
