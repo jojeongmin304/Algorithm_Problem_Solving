4b8b4bf199cfe42180daa4a18229f1ae2b38f228
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-17 11:39:42 +0900

    [level 2] Title: 가장 큰 수, Time: 404.34 ms, Memory: 24.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,17 +1,19 @@
 from functools import cmp_to_key
 
-def cmp (a, b):
+def cmp(a, b):
     if a + b > b + a:
         return -1
-    elif a + b < b + a: 
+    elif a + b < b + a:
         return 1
     else:
         return 0
 
 def solution(numbers):
     answer = ''
-    numbers = list(map(str, numbers))
-    sortedlist = sorted(numbers, key = cmp_to_key(cmp))
-    for c in sortedlist:
-        answer += c
+    strnums = list(map(str, numbers))
+    sortedstrnums = sorted(strnums, key = cmp_to_key(cmp))
+    
+    for t in sortedstrnums:
+        answer += t
+    
     return '0' if answer[0] == '0' else answer
