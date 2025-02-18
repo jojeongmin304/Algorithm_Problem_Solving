eefcd1e5edae817619efc87b46e5980443bd64e0
JeongMin <140388077+jojeongmin304@users.noreply.github.com>
2025-02-18 10:35:59 +0900

    [level 2] Title: 소수 찾기, Time: 91.73 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,34 +1,37 @@
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <algorithm>
-#include <set>
-#include <cmath>
 
 using namespace std;
 
-bool isPrime(int num) {
-    if (num < 2)
+bool isPrime(int a) {
+    if (a == 0 || a == 1)
         return false;
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) return false;
+    
+    for (int i = 2; i < a; i++) {
+        if (a % i == 0) return false;
     }
+    
     return true;
 }
 
 int solution(string numbers) {
     int answer = 0;
-    vector<int> temp;
-    set<int> a{};
-    
     sort(numbers.begin(), numbers.end());
+    unordered_set<int> set;
+    
     do {
         string str = "";
-        for (const auto& c : numbers) {
-            str += c;
-            if (isPrime(stoi(str)))
-                a.insert(stoi(str));
+        for (int i = 0; i < numbers.size(); i++) {
+            str += numbers[i];
+            int num = stoi(str);
+            if (isPrime(num))
+                set.insert(num);
         }
+        
     }while(next_permutation(numbers.begin(), numbers.end()));
     
-    return a.size();
+    
+    return set.size();
 }
